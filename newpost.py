62bf968f5fcce3529fa12c6a7d5e245339c49fe4
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-02-19 12:01:18 -0600

    Split into logical functions.

--- newpost.py
+++ newpost.py
@@ -7,9 +7,26 @@ import argparse
 import subprocess
 import subprocess
 
-if __name__ == '__main__':
+def create_new_post(parent_dir):
     now = datetime.datetime.now()
     date_iso = now.date().isoformat()
+    new_uuid = uuid.uuid4()
+    filename = '{}.rst'.format(new_uuid)
+    filepath = os.path.join(parent_dir, filename)
+    with open(filepath, 'w') as f:
+        f.write(":date: {}\n".format(date_iso))
+        f.write(":slug: {}\n".format(new_uuid))
+    return filepath
+
+def open_with_default_text_editor(filepath):
+    if os.name == 'nt':
+        os.startfile(filepath, 'open')
+        sys.exit(0)
+    elif os.name == 'posix':
+        default_editor = os.getenv('EDITOR', default='vi')
+        subprocess.call([default_editor, filepath])
+
+if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         parser = argparse.ArgumentParser(description='Generates a new post.')
@@ -23,16 +40,6 @@ if __name__ == '__main__':
         sys.stderr.write("Error: not a directory: {}\n".format(post_dir))
         sys.exit(1)
 
-    new_uuid = uuid.uuid4()
-    filename = '{}.rst'.format(new_uuid)
-    filepath = os.path.join(post_dir, filename)
-    with open(filepath, 'w') as f:
-        f.write(":date: {}\n".format(date_iso))
-        f.write(":slug: {}\n".format(new_uuid))
+    post_filepath = create_new_post(post_dir)
 
-    if os.name == 'nt':
-        os.startfile(filepath, 'open')
-        sys.exit(0)
-    elif os.name == 'posix':
-        default_editor = os.getenv('EDITOR', default='vi')
-        subprocess.call([default_editor, filepath])
+    open_with_default_text_editor(post_filepath)
