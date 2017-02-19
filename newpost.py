81904fc2b771ce3e12fd5cd4b445e10cf96c81df
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-02-19 12:32:45 -0600

    Make a custom argparse action for better checking.

--- newpost.py
+++ newpost.py
@@ -26,19 +26,26 @@ def open_with_default_text_editor(filepath):
         default_editor = os.getenv('EDITOR', default='vi')
         subprocess.call([default_editor, filepath])
 
+class ExistingDirectory(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs > 1:
+            raise ValueError("Pass only one directory.")
+        super(ExistingDirectory, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        if not os.path.isdir(value):
+            raise ValueError('Not an existing directory: {}'.format(value))
+        setattr(namespace, self.dest, value)
+
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         parser = argparse.ArgumentParser(description='Generates a new post.')
-        parser.add_argument('post_dir', default=os.getcwd(), help='Path to post directory.')
+        parser.add_argument('post_dir', action=ExistingDirectory, help='Path to post directory.')
         args = parser.parse_args()
         post_dir = args.post_dir
     else:
         post_dir = os.getcwd()
-
-    if not os.path.isdir(post_dir):
-        sys.stderr.write("Error: not a directory: {}\n".format(post_dir))
-        sys.exit(1)
 
     post_filepath = create_new_post(post_dir)
 
